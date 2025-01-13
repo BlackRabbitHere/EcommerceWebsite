9dcd4fc470412f0c8390c60185efbdb979f3d371
Kshitij <kshitijkumar514@gmail.com>
2025-01-13 10:25:43 +0530

    Interface for categories initalized

---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+
+import java.util.List;
+
+// Interface for loose coupling and modularity
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}
