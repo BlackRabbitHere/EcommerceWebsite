2510579940093cc7f8e3459537c1f2a699197d3c
Kshitij <kshitijkumar514@gmail.com>
2025-01-13 11:22:49 +0530

    Delete functionality logic initialized

--- src/main/java/com/ecommerce/project/service/CategoryServiceImp.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImp.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImp implements CategoryService {
     private List<Category> categories=new ArrayList<>();
+    private Long nextId=1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -17,6 +18,19 @@ public class CategoryServiceImp implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long id) { // Logic to delete a user
+        Category category=categories.stream()
+                .filter(c->c.getCategoryId().equals(id))
+                .findFirst().orElse(null);
+        if(category==null){
+            return "Category not found";
+        }
+        categories.remove(category);
+        return "Category with categoryid: "+id+" deleted successfully";
     }
 }
