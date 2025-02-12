ea2c36c44a47f9ce765b39d5eeb9eadf35b55e2b
Kshitij <kshitijkumar514@gmail.com>
2025-02-12 08:59:05 +0530

    Implemented the Delete products delete mapping request Functionality

--- src/main/java/com/ecommerce/project/service/ProductServiceImp.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImp.java
@@ -96,4 +96,12 @@ public class ProductServiceImp implements ProductService{
 
         return modelMapper.map(savedProduct,ProductDTO.class);
     }
+
+    @Override
+    public ProductDTO deleteProduct(Long productId) {
+        Product product=productRepository.findById(productId)
+                .orElseThrow(()-> new ResourceNotFoundException("Product","productId",productId));
+        productRepository.deleteById(productId);
+        return modelMapper.map(product,ProductDTO.class);
+    }
 }
