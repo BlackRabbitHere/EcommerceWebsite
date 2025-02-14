71f2f3677e307dfb29d7f9d2a0436f7a5f5dcfd9
Kshitij <kshitijkumar514@gmail.com>
2025-02-14 19:08:51 +0530

    Implemented the upload Images functionality in the project

--- src/main/java/com/ecommerce/project/service/ProductServiceImp.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImp.java
@@ -11,8 +11,14 @@ import com.ecommerce.project.payload.ProductResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImp implements ProductService{
@@ -104,5 +110,50 @@ public class ProductServiceImp implements ProductService{
                 .orElseThrow(()-> new ResourceNotFoundException("Product","productId",productId));
         productRepository.deleteById(productId);
         return modelMapper.map(product,ProductDTO.class);
+    }
+
+    @Override
+    public ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException {
+        // Get product from DB
+        Product productFromDB=productRepository.findById(productId)
+                .orElseThrow(()-> new ResourceNotFoundException("Product","productId",productId));
+
+        // Upload the image to server
+        // Get the filename of uploaded image
+        String path="images/";
+        String filename=uploadImage(path,image);
+
+        // Updating the new file name to the product
+        productFromDB.setImage(filename);
+
+        //Save updated Product
+        Product updatedProduct=productRepository.save(productFromDB);
+
+        // return DTO after mapping product to DTO
+        return modelMapper.map(updatedProduct,ProductDTO.class);
+    }
+
+    private String uploadImage(String path, MultipartFile file) throws IOException {
+        // File name of current / original file
+        String originalFilename = file.getOriginalFilename();
+
+        // Generate a unique file name (Random UUID)
+        String randomId= UUID.randomUUID().toString();
+        //mat.jgp --> random int is 1234 ---> new file name 1234.jpg
+        String fileName=randomId.concat(originalFilename.substring(originalFilename.lastIndexOf('.')));
+        String filePath=path+ File.separator+fileName;
+
+        // Check if path exists or create
+        File folder=new File(path);
+        if(!folder.exists()){
+            folder.mkdirs();
+        }
+
+
+        //Upload to server
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+
+        //returning file name
+        return fileName;
     }
 }
