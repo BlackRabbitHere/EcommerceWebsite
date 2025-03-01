18c7ca7654e9a331bb045d2b364f8d56bd378ac4
Kshitij <kshitijkumar514@gmail.com>
2025-03-01 09:15:53 +0530

    Created all the repositories of orders and payment

--- src/main/java/com/ecommerce/project/Repositories/AddressRepository.java
+++ src/main/java/com/ecommerce/project/Repositories/AddressRepository.java
@@ -3,9 +3,10 @@ package com.ecommerce.project.Repositories;
 import com.ecommerce.project.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     Address findAddressByAddressId(Long addressId);
