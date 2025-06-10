f6605e39d1d0f8f72d7790c5d914d3976f9a3e3b
Kshitij <kshitijkumar514@gmail.com>
2025-06-10 11:59:56 +0530

    Added the Stripe payment configuration for accepting payments

--- src/main/java/com/ecommerce/project/controller/OrderController.java
+++ src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -3,11 +3,15 @@ package com.ecommerce.project.controller;
 import com.ecommerce.project.payload.OrderDTO;
 import com.ecommerce.project.payload.OrderRequestDTO;
 //import com.ecommerce.project.payload.StripePaymentDto;
+import com.ecommerce.project.payload.StripePaymentDto;
 import com.ecommerce.project.service.OrderService;
 //import com.ecommerce.project.service.StripeService;
+import com.ecommerce.project.service.StripeService;
 import com.ecommerce.project.util.AuthUtil;
 //import com.stripe.exception.StripeException;
 //import com.stripe.model.PaymentIntent;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +27,8 @@ public class OrderController {
     @Autowired
     private AuthUtil authUtil;
 
-//    @Autowired
-//    private StripeService stripeService;
+    @Autowired
+    private StripeService stripeService;
 
     @PostMapping("/order/users/payments/{paymentMethod}")
     public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod
@@ -43,9 +47,9 @@ public class OrderController {
         return new ResponseEntity<>(order, HttpStatus.CREATED);
     }
 
-//    @PostMapping("/order/stripe-client-secret")
-//    public ResponseEntity<String> createStripeClientSecret(@RequestBody StripePaymentDto stripePaymentDto) throws StripeException {
-//        PaymentIntent paymentIntent = stripeService.paymentIntent(stripePaymentDto);
-//        return new ResponseEntity<>(paymentIntent.getClientSecret(), HttpStatus.CREATED);
-//    }
+    @PostMapping("/order/stripe-client-secret")
+    public ResponseEntity<String> createStripeClientSecret(@RequestBody StripePaymentDto stripePaymentDto) throws StripeException {
+        PaymentIntent paymentIntent = stripeService.paymentIntent(stripePaymentDto);
+        return new ResponseEntity<>(paymentIntent.getClientSecret(), HttpStatus.CREATED);
+    }
 }
